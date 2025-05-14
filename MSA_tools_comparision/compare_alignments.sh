180532ab3719655b13555238958556875e84f905
Kimi-chuheng <cxnnd@umsystem.edu>
2025-05-14 14:48:45 -0500

    Update compare_alignments.sh

--- MSA_tools_comparision/compare_alignments.sh
+++ MSA_tools_comparision/compare_alignments.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# compare_alignments.sh - 使用不同工具进行多序列比对
+# compare_alignments.sh - Perform multiple sequence alignments using different tools
 
 input=$1
 base=$(basename "$input" .fasta)
 
-# 运行不同的比对工具
+# Run different alignment tools
 muscle -in $input -out ${base}_muscle.aln
 clustalw2 -infile=$input -outfile=${base}_clustalw.aln -output=clustal
 mafft $input > ${base}_mafft.aln
 
-# 计算比对结果的差异
-echo "比对长度对比："
+# Compare alignment lengths
+echo "Alignment length comparison:"
 for aln in ${base}_*.aln; do
     echo -n "$aln: "
     grep -v "^>" $aln | tr -d ' \n' | wc -c
